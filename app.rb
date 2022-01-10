03b2b2b5bc75a056619810e531370b532fb6bf90
Marcus <marcusventin@gmail.com>
2022-01-10 15:22:46 +0000

    Added cat functionality

--- app.rb
+++ app.rb
@@ -8,3 +8,9 @@ end
 get '/secret' do
   "It's a secret"
 end
+
+get '/cat' do
+  "<div style='border: 5px dashed red'>
+  <img src='https://m.media-amazon.com/images/I/51KNy3fAsgL._AC_.jpg'>
+  </div>"
+end
